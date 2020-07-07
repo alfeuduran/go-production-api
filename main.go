99f58a24971829d7c440ab07ed6e0bfbdba32e67
Alfeu Duran <alfeuduran@gmail.com>
2020-07-07 19:36:23 -0300

    remove handlers

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello()
 	http.ListenAndServe(":9090", nil)
 
 }
-
-#teste 
