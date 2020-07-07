8a4540566e4ee79b2111ad27740079931e266f3d
Alfeu Duran <alfeuduran@gmail.com>
2020-07-07 19:29:26 -0300

    teste handlers

--- main.go
+++ main.go
@@ -7,12 +7,9 @@ import (
 )
 
 func main() {
+
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-
-	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-
-	http.ListenAndServe(":9090", sm)
+	hh := handlers.NewHello()
+	http.ListenAndServe(":9090", nil)
 
 }
