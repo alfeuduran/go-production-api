518492942e0800378bc7ecce0df2696473bb8865
Alfeu Duran <alfeuduran@gmail.com>
2020-07-08 04:26:22 -0300

    add new ServerMux

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/alfeuduran/go-production-api/handlers"
 )
 
 func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello()
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
 	http.ListenAndServe(":9090", sm)
 
