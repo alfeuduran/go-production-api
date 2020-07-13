7a315ac6960f5a91ab89414d384df8771f3b33b9
alfeuduran <alfeuduran@gmail.com>
2020-07-13 15:29:36 -0300

    add new product handler

---
+++ data/products.go
@@ -0,0 +1,39 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+func GetProducts() []*Product {
+	return productList
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Cafe Gelado com leite",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Pequeno e forte sem leite",
+		Price:       1.99,
+		SKU:         "fj34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
--- go.mod
+++ go.mod
@@ -2,7 +2,3 @@ module github.com/alfeuduran/go-production-api
 
 go 1.14
 
-require (
-	github.com/nicholasjackson/building-microservices-youtube/product-api v0.0.0-20200615074401-130c1df925c8
-	github.com/nicholasjackson/env v0.6.0
-)
